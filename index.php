7c2548c70685440690c3c6b3153259c8c4b46d8e
cbaspapu <sebasnarutoxd@gmail.com>
2021-08-15 16:31:04 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOJA DE VIDA</title>
+        
+        
+        <link rel="stylesheet" href="css/style.css">
+        
+        
+        
+    </head>
+    <body>
+        
+        <div id = "Pagina1">
+                        
+                                  
+            <fieldset>
+                
+                <ul>
+                    <li>Fecha de Nacimiento: 15-10-2000</li>
+                    <li>Cedula: 1002948881</li>
+                    <li>Nacionalidad: Colombiano</li>
+                    <li>Estado Civil: Soltero</li>
+                    
+                </ul>
+                
+                
+            </fieldset>
+            <br/>
+                                   
+            
+            
+        </div>
+        
+        <div id = "Pagina2">
+                        
+                                  
+            <fieldset>
+                
+                <ul>
+                    <li>Fecha de Nacimiento: 15-10-2000</li>
+                    <li>Cedula: 1002948881</li>
+                    <li>Nacionalidad: Colombiano</li>
+                    <li>Estado Civil: Soltero</li>
+                    
+                </ul>
+                
+                
+            </fieldset>
+            <br/>
+                                   
+            
+            
+        </div>
+
+
+        
+    </body>
+</html>
