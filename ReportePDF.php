994ae31c3d823ee1b5dc9fe272fe39f906a5a84f
sebaspapu <78824345+sebaspapu@users.noreply.github.com>
2021-08-29 13:35:58 -0500

    Creacion de Archivo "ReportePDF.php"

---
+++ ReportePDF.php
@@ -0,0 +1,289 @@
+<?php ob_start(); ?>
+
+<?php
+/* apartir de aqui empiezo a poner lo que va en el pdf:
+*/
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOJA DE VIDA</title>
+        
+        
+        <link rel="stylesheet" href="css/style.css">
+        
+        
+        
+    </head>
+    <body>
+        
+        <div id = "Pagina1">
+            
+            <div id="marcoGris" >
+                        
+                <img src="img/Foto.jpg" width="120" hspace="80" height="139.2" vspace="30"'/>                
+                <h1 id="nombre">SEBASTIAN BOLAÑOS MORALES</h1>
+                <p id="titulacion">Tnlgo. SISTEMAS DE LA INFORMACIÓN</p>
+                
+                <div id="barraTitulacion"></div>
+
+                <p id="tituloPerfilProfesional">PERFIL PROFESIONAL</p>
+
+                <p class="parrafoPerfilProfesional">Dentro de mis intereses y actitudes están la responsabilidad y el respeto hacia mis compañeros, y demás personal, además de una excelente facilidad para dominar cualquier tarea que se me asigne. Con lo aprendido en la universidad y mi experiencia laboral en la misma e logrado identificar diferentes estrategias y competencias que me permitirán a nivel ocupacional mejorar cada día, crecer en diferentes áreas, fijar metas personales y laborales con las cuales pueda aportar de manera positiva a las empresas.</p>
+
+                <p id="tituloHabilidades">HABILIDADES BLANDAS</p>
+
+                <ul class="Habilidades">
+                    <li>Adaptabilidad.</li>
+                    <li>Resolución de problemas.</li>
+                    <li>Perseverancia.</li>
+                    <li>Empatía.</li>
+                    <li>Tolerante al estrés.</li>
+                    <li>Trabajo en equipo.</li>
+                    <li>Creatividad e innovación.</li>
+                    <li>Proactividad.</li>
+                    <li>Pensamiento analítico.</li>
+                    <li>Asertividad.</li>
+                </ul>
+
+                <p id="tituloDatosPersonales">DATOS PERSONALES</p>
+
+                <ul class="datosPersonales">
+                    <b>Documento:</b> 1002948881 Caloto, Cauca<br>
+                    <b>Fecha de nacimiento:</b> 15 de Octubre de
+                    2000<br>
+                    <b>Dirección actual:</b>Cra 5 #1-319, Caloto
+                    Cauca<br>
+                    <b>Celular:</b> 301 4412780<br>
+                    <b>Correo:</b> sebas_08_200@yahoo.es<br>
+                    <b>Perfil LinkedIn:</b> <a id="link1" href="https://linkedin.com/in/sebas-bolaños-2021">linkedin.com/in/sebas-bolaños-2021</a><br>
+                </ul>
+
+            </div>
+
+            <div id="barraHistorialAcademico">
+                <p class="historialAca">HISTORIAL ACADÉMICO</p>                
+            </div>
+            
+            <div id="cajaHistorialAcademico">
+
+                <p class="estudiosP">ESTUDIOS DE PREGRADO</p>
+
+                <p class="estudios">TECNOLOGO EN SISTEMAS DE LA INFORMACIÓN. UNIVERSIDAD DEL VALLE</p>
+                <p class="estudiosInfo">Del 2017 al 2021<br>
+                    En espera de diploma<br>
+                    Cali, Valle del Cauca<br>
+                </p>
+
+                <p class="estudiosS">TECNICO EN SOPORTE DE REDES Y SISTEMAS INFORMATICOS, Intro Progress System Plus Ltda.</p>
+                <p class="estudiosInfoS">Del 2012 al 3 de Mayo del 2014<br>
+                    Graduado<br>
+                    Santander de Quilichao, Cauca<br>
+                </p>
+
+                <p class="estudiosPE">ESTUDIOS SECUNDARIOS</p>
+
+                <p class="estudiosACA">BACHILLER ACADÉMICO, INSTITUCION EDUCATIVA NUCLEO ESCOLAR RURAL CALOTO</p>
+                <p class="estudiosInfoACA">Del 2011 al 11 de Diciembre del 2016<br>
+                    Graduado<br>
+                    Caloto, Cauca<br>
+                </p>
+
+                <p class="estudiosPE">CURSOS (SENA)</p>
+
+                <p class="estudiosACA">COMPLEMENTARIA VIRTUAL EN ENGLISH DOT WORKS BEGINNER - INGLÉS</p>
+                <p class="estudiosInfoACA">60 HORAS - Certificado<br>
+                    22 de Junio del 2021<br>
+                    Barranquilla, Atlántico<br>
+                </p>
+
+                <p class="estudiosSA">COMPLEMENTARIA VIRTUAL EN CONTROLES Y SEGURIDAD INFORMATICA</p>
+                <p class="estudiosInfoSA">40 HORAS - En curso<br>
+                    Agosto del 2021<br>
+                    Ibagué, Tolima<br>
+                </p>
+
+                <p class="estudiosLA">CURSOS (LINKEDIN)</p>
+
+                <p class="estudiosL">GITHUB PARA PROGRAMADORES</p>
+                <p class="estudiosInfoL">1 Hora 53min - Certificado<br>
+                    11 de Junio del 2021<br>
+                    Sunnyvale, CA 94085<br>
+                </p>
+
+                <p class="estudiosXY">LOGROS Y ESTIMULOS ACADÉMICOS</p>
+
+                <p class="estudiosX">ESTIMULO POR BUEN DESEMPEÑO ACADÉMICO UNIVERSIDAD DEL VALLE</p>
+                <p class="estudiosInfoX">Semestre Agosto-Diciembre 2017<br>
+                    Cali, Valle del Cauca<br>
+                </p>
+
+                <p class="estudiosY">ESTIMULO POR BUEN DESEMPEÑO ACADÉMICO UNIVERSIDAD DEL VALLE</p>
+                <p class="estudiosInfoY">Semestre Febrero-Junio 2018<br>
+                    Cali, Valle del Cauca<br>
+                </p>
+
+                <p class="estudiosZ">ESTIMULO POR BUEN DESEMPEÑO ACADÉMICO UNIVERSIDAD DEL VALLE</p>
+                <p class="estudiosInfoZ">Semestre Junio-Octubre 2020<br>
+                    Cali, Valle del Cauca<br>
+            </p>
+
+            </div>
+            
+        </div>    
+
+        <div id = "Pagina2">
+                  
+            
+            <div id="marcoGris" >
+                      
+                <img src="img/Foto.jpg" width="120" hspace="80" height="139.2" vspace="30"'/>                
+                <h1 id="nombre">SEBASTIAN BOLAÑOS MORALES</h1>
+                <p id="titulacion">Tnlgo. SISTEMAS DE LA INFORMACIÓN</p>
+
+                <div id="barraTitulacion"></div>
+
+                <p id="tituloPerfilOcupacional">PERFIL OCUPACIONAL</p>
+
+                <p class="parrafoPerfilOcupacional">Cuento con conocimientos en programación (POO) Java, Y conocimientos en lenguajes como, C++, PHP, Python y software de control de versiones Git/GitHub. Poseo conocimientos en Bases de Datos (Lenguaje SQL) y en Administración de Servidores (Microsoft Windows Server, Linux/CentOS). También cuento con la facultad para desempeñarme en área de soporte y mantenimiento de PCs. Actualmente continúo profundizando y aprendiendo con el fin de agilizar y optimizar procesos en pro del desarrollo en conjunto.</p>
+
+                <p id="tituloHabilidades">HABILIDADES BLANDAS</p>
+
+                <ul class="Habilidades">
+                    <li>Adaptabilidad.</li>
+                    <li>Resolución de problemas.</li>
+                    <li>Perseverancia.</li>
+                    <li>Empatía.</li>
+                    <li>Tolerante al estrés.</li>
+                    <li>Trabajo en equipo.</li>
+                    <li>Creatividad e innovación.</li>
+                    <li>Proactividad.</li>
+                    <li>Pensamiento analítico.</li>
+                    <li>Asertividad.</li>
+                </ul>
+
+                <p id="tituloDatosPersonales">DATOS PERSONALES</p>
+
+                <ul class="datosPersonales">
+                    <b>Documento:</b> 1002948881 Caloto, Cauca<br>
+                    <b>Fecha de nacimiento:</b> 15 de Octubre de
+                    2000<br>
+                    <b>Dirección actual:</b>Cra 5 #1-319, Caloto
+                    Cauca<br>
+                    <b>Celular:</b> 301 4412780<br>
+                    <b>Correo:</b> sebas_08_200@yahoo.es<br>
+                    <b>Perfil LinkedIn:</b> <a id="link1" href="https://linkedin.com/in/sebas-bolaños-2021">linkedin.com/in/sebas-bolaños-2021</a><br>
+                </ul>
+
+
+            </div>
+
+            <div id="barraExperienciaLaboral">
+                <p class="ExperienciaLaboral">EXPERIENCIA LABORAL</p>                
+            </div>
+           
+            <div id="cajaExperienciaLaboral">
+
+                <p class="tituloMon">MONITOR GESTIÓN TECNOLOGICA</p>
+
+                <ul class="datosPersonalesx">
+                    Universidad del Valle – Sede Norte del Cauca.<br>
+                    <li><b>Funciones:</b> Brindar soporte de carácter tecnológico, en materia de conectividad a estudiantes y profesorado de la Universidad del Valle.<br></li>
+                    <li><b>Jefe Inmediato:</b> Paula Andrea Figueroa Polanco<br></li>
+                    <li><b>Dirección:</b> Carrera 13 No. 19 - 231 Vía Cali-Popayán<br></li>
+                    Santander de Quilichao, Colombia<br>
+                    <li><b>Ofic. Académica:</b> 57 2 8298718<br></li>
+                    <li><b>Tiempo laborado:</b> Junio a Octubre del 2020<br></li>
+                </ul>
+
+                <p class="tituloGestion"><b>Proyectos personales (Freelance)</b></p>
+
+                <p class="parrafoProyectosPersonales">He realizado diversos proyectos con el lenguaje JAVA, en donde destaco un sistema de asignación de citas para una EPS y haciendo uso del IDE Android Studio desarrolle una app que permite detectar y trazar rutas a establecimientos de comidas rápidas previamente registrados. Por otro lado, también he realizado pequeños proyectos con PHP, HTML Y CSS.</p>
+                    <p class="parrafoContinuacion">Todos estos proyectos los pueden encontrar en mis repositorios de GitHub: <a id="link2" href="https://github.com/sebaspapu">https://github.com/sebaspapu</a>
+                    </p>
+
+            </div>
+
+            <div id="barraReferenciasPersonales">
+                <p class="ReferenciasPersonales">REFERENCIAS PERSONALES</p>                
+            </div>
+
+            <div id="cajaReferenciasPersonales">
+                
+                <p class="tituloUno"><b>Jorge Deison Arias Mina</b></p>
+                <p class="parrafoUno">Trabajador Bienestar Familiar<br>
+                    Celular: 3058605100<br>
+                </p>
+            
+                <p class="tituloDos"><b>Luis Carlos Bedoya</b></p>
+                <p class="parrafoDos">Trabajadora Independiente<br>
+                    Celular: 3137878398<br>
+                </p>
+
+                <p class="tituloTres"><b>Mario Daza</b></p>
+                <p class="parrafoTres">Trabajador Empresa Carvajal<br>
+                    Celular: 3164075271<br>
+                </p>
+                
+            </div>
+
+            <div id="barraIdiomas">
+                <p class="Idiomas">IDIOMAS</p>                
+            </div>
+
+            <div id="cajaIdiomas">
+
+                <p class="tituloUnoIn"><b>INGLES</b></p>
+
+                <p class="TextoIdiomas"><b>Habla:</b> Principiante (A2)<br>
+                    <b>Escribe:</b> Principiante (A2)<br>
+                    <b>Lee:</b> Intermedio (B1)<br>
+                    <b>Entiende:</b> Intermedio (B1)<br>
+                    <b>Lugar Apredzaje:</b> Universidad del Valle</p>
+                
+            </div>
+
+
+        </div>   
+
+        
+    </body>
+</html>
+
+
+
+
+
+
+
+ <?php
+/* apartir de aqui empiezo a poner que genera el pdf:
+*/
+?>
+
+
+<?php
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', true );
+//$pdf=new FPDF('P', 'mm', '300, 200');  
+
+require_once 'dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+$dompdf = new DOMPDF();
+$dompdf->load_html(ob_get_clean());
+$dompdf->render();
+$pdf = $dompdf->output();
+$filename = "profesionales.pdf";
+file_put_contents($filename, $pdf);
+$dompdf->stream($filename);
+
+?>
